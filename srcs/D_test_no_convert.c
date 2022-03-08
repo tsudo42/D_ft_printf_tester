d08eae6df6fe435515ad1851731e40f009737ba8
tsudo42 <91622558+tsudo42@users.noreply.github.com>
2022-03-08 15:56:13 +0900

    Removed memory check from `test_no_convert`
    because it causes error on guacamole.

--- srcs/D_test_no_convert.c
+++ srcs/D_test_no_convert.c
@@ -6,26 +6,11 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 14:47:49 by tsudo             #+#    #+#             */
-/*   Updated: 2022/03/08 15:36:22 by tsudo            ###   ##########        */
+/*   Updated: 2022/03/08 15:55:58 by tsudo            ###   ##########        */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "D_test.h"
-
-static void	test_memory(void)
-{
-	int			ret;
-	char		buf1[D_BUFFER_SIZE];
-	char		buf2[D_BUFFER_SIZE];
-	const char	*str = "abcdef %% ghijklmn %% opqrstu";
-
-	memset(buf1, 'A', D_BUFFER_SIZE);
-	strcpy(buf1 + 10, str);
-	memcpy(buf2, buf1, D_BUFFER_SIZE);
-	TEST_NC(buf1);
-	ret = memcmp(buf1, buf2, D_BUFFER_SIZE);
-	printf("\nmemcmp : %d\n", ret);
-}
 
 void	test_no_convert(void)
 {
@@ -33,11 +18,9 @@ void	test_no_convert(void)
 	TEST_NC("a");
 	TEST_NC("%% %%");
 	TEST_NC("abcdef %% ghijklmn %% opqrstu");
-	test_memory();
 }
 
 void	test_no_convert_bonus(void)
 {
 	test_no_convert();
-	test_memory();
 }
