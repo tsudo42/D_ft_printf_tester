14493b035457f5c9f8b85f247a1ba0c7e74d2ba5
tsudo <tsudo@c1r14s4.42tokyo.jp>
2022-06-24 23:12:33 +0900

    Add bonus func calls to main

--- srcs/D_main_bonus.c
+++ srcs/D_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tsudo <tsudo@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/10 14:20:10 by tsudo             #+#    #+#             */
-/*   Updated: 2022/03/08 16:28:43 by tsudo            ###   ##########        */
+/*   Updated: 2022/06/24 23:08:41 by tsudo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@
 int	main(void)
 {
 	test_hand_bonus();
-	test_p_bonus();
 	test_no_convert_bonus();
+	test_c_bonus();
+	test_s_bonus();
+	test_p_bonus();
+	test_d_bonus();
+	test_i_bonus();
+	test_u_bonus();
+	test_x_bonus();
 	return (0);
 }
